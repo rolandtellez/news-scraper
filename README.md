d925e685735c2bacba6fd1d444b290986cd5845f
rawlycopter <roland.tellez819@gmail.com>
2019-06-22 15:50:09 -0700

    readme update and server update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # news-scraper
+
+Scrapes news from the satire site, "The Hard Times", links to the actual articles, and allows users to add and save comments to the Mongo database.
--- server.js
+++ server.js
@@ -32,7 +32,7 @@ mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
 // Routes
 
 // A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+app.get("/", function(req, res) {
   // First, we grab the body of the html with axios
   axios.get("https://www.thehardtimes.net/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
