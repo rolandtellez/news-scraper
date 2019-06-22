3b82094b811b62aafa19451cc98df4a8b93fc98d
rawlycopter <roland.tellez819@gmail.com>
2019-06-22 16:08:56 -0700

    server update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-var PORT = 3000;
+var port = 3000;
 
 // Initialize Express
 var app = express();
@@ -133,6 +133,6 @@ app.post("/articles/:id", function(req, res) {
 });
 
 // Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+app.listen(port, function() {
+  console.log("App running on port " + port + "!");
 });
